951ef3f262605b0342f07cc27edfbbeca5d8507b
karladuran28 <kaduran1998@gmail.com>
2025-01-26 22:46:45 -0500

    Angular Skeleton

---
+++ DatingApp/client/src/app/app.component.ts
@@ -0,0 +1,24 @@
+import { NgFor } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [NgFor],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent implements OnInit{
+  http = inject(HttpClient);
+  title = 'DatingApp';
+  users: any;
+
+  ngOnInit(): void {
+    this.http.get('https://localhost:5001/api/users').subscribe({
+      next: response => this.users = response,
+      error: error => console.error(error),
+      complete: () => console.log('Request has completed')
+    })
+  }
+}
