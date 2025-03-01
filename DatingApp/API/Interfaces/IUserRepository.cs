a732fcefe42d243b659d570b5665fab987b2c7ec
karladuran28 <kaduran1998@gmail.com>
2025-03-01 17:33:28 -0500

    Adding signals for members

--- DatingApp/API/Interfaces/IUserRepository.cs
+++ DatingApp/API/Interfaces/IUserRepository.cs
@@ -9,7 +9,7 @@ public interface IUserRepository
   Task<bool> SaveAllAsync();
   Task<IEnumerable<AppUser>> GetUsersAsync();
   Task<AppUser?> GetUserByIdAsync(int id);
-  Task<AppUser?> GetUserByUsernameAync(string username);
+  Task<AppUser?> GetUserByUsernameAsync(string username);
   Task<IEnumerable<MemberDto>> GetMembersAsync();
   Task<MemberDto?> GetMemberAsync(string username);
 }
